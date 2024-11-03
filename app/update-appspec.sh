c4d27a40456bab888cbae72fefdf3f13b018371c
Andy Luis <mvpjava.email@gmail.com>
2024-11-03 21:17:46 +0000

    extract task definition data like arn, docker container name and port and substitute those values in appspec file for deployments

--- app/update-appspec.sh
+++ app/update-appspec.sh
@@ -2,23 +2,45 @@
 
 echo "$0 started"
 
+APPSPEC_FILE="./appspec.json"
 TASK_DEFN_NAME=my-nginx-task
+
+if [ "$#" -ne 1 ]; then
+  echo "task definition name not specified, using default value of 'my-nginx-task'"
+else
+  TASK_DEFN_NAME=$1
+fi
 
 TASK_ARN=`aws ecs describe-task-definition \
     --task-definition $TASK_DEFN_NAME \
     --query 'taskDefinition.taskDefinitionArn' \
     --output text`
 
-echo "TASK Definition ARN: $TASK_ARN"
+echo "Updating $APPSPEC_FILE with TaskDefinition: $TASK_ARN"
 
-aws ecs describe-task-definition \
+sed -i "s|\"TaskDefinition\": \"arn:.*\"|\"TaskDefinition\": \"$TASK_ARN\"|" "$APPSPEC_FILE"
+
+
+CONTAINER_NAME=`aws ecs describe-task-definition \
     --task-definition $TASK_DEFN_NAME \
     --query 'taskDefinition.containerDefinitions[].name' \
-    --output text
+    --output text`
 
-aws ecs describe-task-definition \
+echo "Updating $APPSPEC_FILE with ContainerName: $CONTAINER_NAME"
+
+sed -i "s|\"ContainerName\": .*\"|\"ContainerName\": \"$CONTAINER_NAME\"|" "$APPSPEC_FILE"
+
+
+CONTAINER_PORT=`aws ecs describe-task-definition \
     --task-definition $TASK_DEFN_NAME \
     --query 'taskDefinition.containerDefinitions[].portMappings[].containerPort' \
-    --output text
+    --output text`
+
+echo "Updating $APPSPEC_FILE with ContainerPort: $CONTAINER_PORT"
+
+sed -i "s|\"ContainerPort\": .*\"|\"ContainerPort\": \"$CONTAINER_PORT\"|" "$APPSPEC_FILE"
+
+echo "Dumping $APPSPEC_FILE"
+cat $APPSPEC_FILE
 
 echo "$0 complete."
