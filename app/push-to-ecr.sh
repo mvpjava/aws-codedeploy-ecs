9f16fd06a67d5fa6917b69c629a9b1ff550c0466
Andy Luis <mvpjava.email@gmail.com>
2024-11-03 16:57:21 +0000

    Add script to push specified image to ecr registry

---
+++ app/push-to-ecr.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+clear
+
+echo "Starting $0 to push image to ECR registry"
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <Image name with tagged version. Example:  403177882230.dkr.ecr.eu-west-2.amazonaws.com/my-nginx:1.0>"
+  exit 1
+fi
+
+# Retrieve an authentication token and authenticate your Docker client to your registry. Use the AWS CLI:
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 403177882230.dkr.ecr.eu-west-2.amazonaws.com
+
+docker push 403177882230.dkr.ecr.eu-west-2.amazonaws.com/my-nginx:latest
+
+echo "$0 Complete."
