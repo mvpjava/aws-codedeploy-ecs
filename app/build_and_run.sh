43cba0c37a6d8147a7eec5360be5a9f0bcfa96b9
Andy Luis <mvpjava.email@gmail.com>
2024-11-03 16:36:33 +0000

    Added docker build script to specify version number in image tag and then update the index.html with theat version number

--- app/build_and_run.sh
+++ app/build_and_run.sh
@@ -1,4 +1,35 @@
 #!/bin/sh
 
-docker build -t my-nginx:latest .
-docker run -it --rm -d -p 8081:80 --name my-nginx-container my-nginx:latest
+clear
+
+HTML_FILE="index.html"
+
+# If a version parameter is provided, use it; otherwise, read it from the file
+if [ -n "$1" ]; then
+    VERSION="$1"
+else
+    VERSION=1.0
+fi
+
+
+# Update the version in index.html
+sed -i "s/Version: [0-9.]\+/Version: $VERSION/" "$HTML_FILE"
+
+# Build and run the Docker container with the specified version tag
+docker build -t "my-nginx:$VERSION" .
+
+#Tag it for ECR registry which has already been set up
+docker tag "my-nginx:$VERSION" "403177882230.dkr.ecr.eu-west-2.amazonaws.com/my-nginx:$VERSION"
+
+#Listing all docker images with name my-nginx in it
+docker images | grep my-nginx
+
+# Clean up any containers left around from previous runs since container name is hardcoded
+# This is just from local testing used for demo purposes
+docker container rm -f my-nginx-container 2>/dev/null
+
+docker run -it --rm -d -p 8081:80 --name "my-nginx-container" "my-nginx:$VERSION"
+
+# Output the version being used
+echo "Built and ran Docker image with version: $VERSION"
+exit 0
